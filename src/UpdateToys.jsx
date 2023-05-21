42c7adb394ba6dd7a0cf7eab14c0a007d77aa1b8
habib-rahmanhabib <mdhabiburrahman80680@gmail.com>
2023-05-21 23:24:12 +0600

    edit form added

--- src/UpdateToys.jsx
+++ src/UpdateToys.jsx
@@ -5,16 +5,14 @@ const UpdateToys = () => {
         event.preventDefault();
         const form = event.target;
 
-        const name = form.name.value;
-        const quantity = form.quantity.value;
-        const supplier = form.supplier.value;
-        const taste = form.taste.value;
-        const category = form.category.value;
-        const details = form.details.value;
-        const photo = form.photo.value;
 
-        const newCoffe = {name, quantity, supplier, taste, category, details, photo};
-        console.log(newCoffe);
+        const quantity = form.quantity.value;
+        const price = form.price.value;
+        const details = form.details.value;
+        console.log(quantity, price, details)
+
+
+
     }
 
     return (
@@ -23,82 +21,39 @@ const UpdateToys = () => {
                 <div className="flex mb-8">
                     <div className="form-control md:w-1/2">
                         <label className="label">
-                            <span className="label-text text-1xl font-semibold"> Name</span>
+                            <span className="label-text text-1xl font-semibold">Price</span>
                         </label>
                         <label className="input-group ">
 
-                            <input type="text" placeholder="Enter Coffe name" name="name" className="input input-bordered w-full" />
+                            <input type="text" placeholder="Update Price" name="price" className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ms-4">
                         <label className="label">
-                            <span className="label-text text-1xl font-semibold">Chef</span>
+                            <span className="label-text text-1xl font-semibold">available quantity</span>
                         </label>
                         <label className="input-group">
 
-                            <input type="text" placeholder="Enter Coffe Chef" name="quantity" className="input input-bordered w-full" />
+                            <input type="text" placeholder="Update Quantity" name="quantity" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
-                {/* supply section */}
+               
                 <div className="flex mb-8">
-                    <div className="form-control md:w-1/2">
+                    <div className="form-control w-full">
                         <label className="label">
-                            <span className="label-text text-1xl font-semibold">Supplier</span>
+                            <span className="label-text text-1xl font-semibold">Details Drescription</span>
                         </label>
                         <label className="input-group ">
 
-                            <input type="text" placeholder="Enter Coffe Supplier" name="supplier" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ms-4">
-                        <label className="label">
-                            <span className="label-text text-1xl font-semibold">Taste</span>
-                        </label>
-                        <label className="input-group">
-
-                            <input type="text" placeholder="Enter Coffe Taste" name="taste" className="input input-bordered w-full" />
+                            <input type="text" placeholder="Updatae Details" name="details" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
 
-                {/* catagorry section */}
-                <div className="flex mb-8">
-                    <div className="form-control md:w-1/2">
-                        <label className="label">
-                            <span className="label-text text-1xl font-semibold">Catagory</span>
-                        </label>
-                        <label className="input-group ">
-
-                            <input type="text" placeholder="Enter Coffe Catagory" name="category" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                    <div className="form-control md:w-1/2 ms-4">
-                        <label className="label">
-                            <span className="label-text text-1xl font-semibold">Details</span>
-                        </label>
-                        <label className="input-group">
-
-                            <input type="text" placeholder="Enter Coffe Details" name="details" className="input input-bordered w-full" />
-                        </label>
-                    </div>
-                </div>
-                {/* details section */}
 
 
-                <div className="form-control md:w-full mb-8">
-                    <label className="label">
-                        <span className="label-text text-1xl font-semibold">Photo</span>
-                    </label>
-                    <label className="input-group ">
-
-                        <input type="text" placeholder="Enter Photo URL" name="photo" className="input input-bordered w-full" />
-                    </label>
-                </div>
-
-
-
-                <input type="submit" value="Add Coffe" className="btn btn-block" />
+                <input type="submit" value="Submit" className="btn btn-block bg-gray-300" />
 
             </form>
 
